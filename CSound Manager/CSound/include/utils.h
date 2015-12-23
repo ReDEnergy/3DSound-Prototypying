f678dfce643f84002873c8e1378347d69b10ead7
Gabriel Ivanica <gabriel.ivanica@gmail.com>
2015-12-23 23:38:34 +0200

    added new BITSET function

--- CSound Manager/CSound/include/utils.h
+++ CSound Manager/CSound/include/utils.h
@@ -6,8 +6,9 @@
 #define SAFE_FREE(p)		if (p) {delete p; p = NULL;}
 #define SAFE_FREE_ARRAY(p)	if (p) {delete[] p; p = NULL;}
 
-#define SET_BIT(item, bit) (item) |= (1 << (bit))
-#define CLEAR_BIT(item, bit) (item) &= ~(1 << (bit))
+#define SET_BIT(item, bit)		(item) |= (1 << (bit))
+#define GET_BIT(item, bit)		(item) & (1 << (bit))
+#define CLEAR_BIT(item, bit)	(item) &= ~(1 << (bit))
 
 typedef unsigned int uint;
 typedef unsigned char uchar;
