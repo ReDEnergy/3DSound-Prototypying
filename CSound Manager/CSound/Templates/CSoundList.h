376dd24073f984bea5aac5b7d2fac001e15e2fab
Gabriel Ivanica <gabriel.ivanica@gmail.com>
2015-12-23 23:37:43 +0200

    added support for individual component requests

--- CSound Manager/CSound/Templates/CSoundList.h
+++ CSound Manager/CSound/Templates/CSoundList.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <list>
+#include <unordered_map>
 
 template <class T, class P>
 class CSoundList
@@ -9,6 +10,7 @@ class CSoundList
 		virtual ~CSoundList() {};
 
 		virtual list<T*>& GetEntries();
+		virtual T* GetEntry(const char* name);
 
 		virtual void Add(T *entry);
 		virtual void Remove(T *entry);
@@ -25,6 +27,7 @@ class CSoundList
 	protected:
 		bool update = true;
 		list<T*> entries;
+		unordered_map<string, T*> entry_map;
 };
 
 
@@ -34,9 +37,18 @@ inline list<T*>& CSoundList<T, P>::GetEntries()
 	return entries;
 }
 
+template<class T, class P>
+inline T * CSoundList<T, P>::GetEntry(const char * name)
+{
+	if (entry_map.find(name) != entry_map.end())
+		return entry_map[name];
+	return nullptr;
+}
+
 template <class T, class P>
 inline void CSoundList<T, P>::Add(T * entry)
 {
+	entry_map[entry->GetName()] = entry;
 	entries.push_back(entry);
 	entry->SetParent(static_cast<P*>(this));
 
@@ -48,6 +60,7 @@ inline void CSoundList<T, P>::Add(T * entry)
 template <class T, class P>
 inline void CSoundList<T, P>::Remove(T * entry)
 {
+	entry_map.erase(entry->GetName());
 	entries.remove(entry);
 	entry->SetParent(static_cast<P*>(nullptr));
 
